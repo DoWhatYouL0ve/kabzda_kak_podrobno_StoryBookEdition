d3470cf763d44e169a1a6adcaf82ec5c6ed4ca7b
DoWhatYouL0ve <william_ross@mailfence.com>
2022-05-12 15:21:55 +0200

    15 lesson Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Select} from "./Select";
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -7,10 +8,41 @@ export default {
     component: Select,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
     argTypes: {
-        backgroundColor: { control: 'color' },
+        backgroundColor: {control: 'color'},
     },
 }
 
+let items = [
+    {title: 'Wroclaw', itemValue: '1'},
+    {title: 'Berlin', itemValue: '2'},
+    {title: 'Monaco', itemValue: '3'},
+    {title: 'Madrid', itemValue: '4'},
+    {title: 'Porto', itemValue: '5'},
+]
+
+
+export const SelectSB = () => {
+    const [value, setValue] = useState('1')
+
+    return <>
+        < Select
+            value={value}
+            onChange={setValue}
+            items={items}
+        />
+    </>
+}
+export const SelectSBWithoutValue = () =>{
+    const [value, setValue] = useState(null)
+
+    return <>
+        < Select
+            value={value}
+            onChange={setValue}
+            items={items}
+        />
+    </>
+}
 
 /*export const ControlledSelect = () => {
     const [parentValue, setParentValue] = useState<string | undefined>(undefined)
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,20 +1,73 @@
-import React from "react";
+import React, {KeyboardEvent, useEffect, useState} from "react";
+import styles from './Select.module.css'
 
 export type ItemSelectType = {
     title: string
     itemValue: any
 }
 export type SelectType = {
-    value: any
+    value?: any
     onChange: (value: any) => void
     items: ItemSelectType[]
 }
 
 export const Select: React.FC<SelectType> = ({value, onChange, items}) => {
+    const [active, setActive] = useState<boolean>(false)
+    const [hoveredItem, setHovered] = useState<any>(value)
+
+    useEffect(() => {
+        setHovered(value)
+    }, [value])
+
+    const selectedValue = items.find(i => i.itemValue === value)
+    const hoveredValue = items.find(i => i.itemValue === hoveredItem)
+    const onClickHandler = () => {
+        setActive(!active)
+    }
+    const onItemClicked = (value: any) => {
+        onChange(value)
+        setActive(!active)
+    }
+
+    const onKeyHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        if(e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for(let i = 0; i < items.length; i++) {
+                if(items[i].itemValue === hoveredItem) {
+                    const nextElement = e.key === 'ArrowDown' ? items[i+1] : items[i-1]
+                    if(nextElement) {
+                        onChange(nextElement.itemValue)
+                        return
+                    }
+                }
+            }
+            if(!selectedValue) {
+                onChange(items[0].itemValue)
+            }
+        }
+        if(e.key === 'Escape' || e.key === 'Enter') {
+            setActive(false)
+        }
+    }
+
+
     return (
         <>
-            <div>{value}</div>
-            {items.map( i => <div>{i.title}</div>)}
+            <div className={styles.selectContainer} onKeyDown={onKeyHandler} tabIndex={0}>
+                <span className={styles.value} onClick={onClickHandler}>{selectedValue && selectedValue.title}</span>
+                {active &&
+                    <div className={styles.items}>
+                        {items.map(i => <div
+                            className={`${styles.item} ${hoveredValue === i ? styles.selected : ''}`}
+                            onClick={() => onItemClicked(i.itemValue)}
+                            key={i.itemValue}
+                            onMouseEnter={()=>setHovered(i.itemValue)}
+                        >
+                            {i.title}
+                        </div>)}
+                    </div>
+                }
+            </div>
         </>
+
     )
 }
