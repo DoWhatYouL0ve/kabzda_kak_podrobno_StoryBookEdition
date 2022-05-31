5dabc6975d75299f617d8c5d32265c2021b39193
DoWhatYouL0ve <william_ross@mailfence.com>
2022-05-31 17:24:19 +0200

    22 lesson useEffect with setTimeout and setInterval examples

--- src/components/useEffect/UseEffect.stories.tsx
+++ src/components/useEffect/UseEffect.stories.tsx
@@ -19,14 +19,40 @@ export const Example_1 = () => {
         console.log('useEffect called every time component render')
         //api.getUsers().then()
         // setInterval
-
-    }, [])
+        document.title = counter.toString()
+    })
     useEffect(() => {
         console.log('useEffect called every time counter changed')
         //api.getUsers().then()
         // setInterval
 
     }, [counter])
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}> + </button>
+        {counter}
+    </>
+}
+
+export const SetTimeOutExample = () => {
+    console.log('function with timeout has been called')
+    let [counter, setCounter] = useState(1)
+
+    // starts working only after jsx has been rendered
+    useEffect(() => {
+        console.log('useEffect called only once when component render')
+        //api.getUsers().then()
+        // setInterval
+        document.title = counter.toString()
+        /*setTimeout(()=>{
+            alert('Hi')
+        }, 1000)*/
+        //using callback inside makes setCounter rewriting old counter value with a new value
+        setInterval(()=>{
+            setCounter(state =>state + 1)
+        }, 1000)
+    },[])
+
 
     return <>
         <button onClick={() => setCounter(counter + 1)}> + </button>
